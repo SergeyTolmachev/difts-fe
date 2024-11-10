e27dbf3090d919f61e50186793ede9d8d62bf9e1
Vsevolod-Rusinskiy <100481804+Vsevolod-Rusinskiy@users.noreply.github.com>
2024-11-10 14:47:30 +0300

    chore

--- src/pages/GiftPage/GiftComponent.jsx
+++ src/pages/GiftPage/GiftComponent.jsx
@@ -10,15 +10,18 @@ import { useNavigate } from "react-router-dom";
 const fetchCountries = async () => {
   const headers = {
     "Content-Type": "application/json",
-    'ngrok-skip-browser-warning': 'open',
+    "ngrok-skip-browser-warning": "open",
     Authorization:
       "user=%7B%22id%22%3A1635638906%2C%22first_name%22%3A%22Narek%22%2C%22last_name%22%3A%22Shaghoyan%22%2C%22username%22%3A%22narek_shaghoyan%22%2C%22language_code%22%3A%22ru%22%2C%22is_premium%22%3Atrue%2C%22allows_write_to_pm%22%3Atrue%7D&chat_instance=2486925991575857822&chat_type=private&auth_date=1731179557&hash=9431b5b46f2542d99357b7cce0b1cd728865ca38b45988930842b94151a88eb9",
   };
 
   try {
-    const response = await axios.get("https://b526-134-209-85-35.ngrok-free.app/api/countries", {
-      headers,
-    });
+    const response = await axios.get(
+      "https://b526-134-209-85-35.ngrok-free.app/api/countries",
+      {
+        headers,
+      }
+    );
     return response.data;
   } catch (error) {
     console.error("Ошибка при получении стран:", error);
@@ -89,7 +92,7 @@ export const GiftComponent = () => {
           ))}
         </div>
       </BottomModal>
-      <TonConnectButton className={styles.connect_wallet_button} />
+      {/* <TonConnectButton className={styles.connect_wallet_button} /> */}
     </div>
   );
 };
